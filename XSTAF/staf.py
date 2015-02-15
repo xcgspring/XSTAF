71137e1c0643156132943775c5368470def446bb
xcgspring <xcgspring@163.com>
2015-02-15 16:53:08 +0800

    draft for staf class
    Signed-off-by: xcgspring <xcgspring@163.com>

--- XSTAF/staf.py
+++ XSTAF/staf.py
@@ -16,7 +16,8 @@ class STAF(object):
         self.staf_handle_id = 0
         self.staf_handle = None
         
-        self.client_daemon = ""
+        #staf monitor name
+        self.monitor_name = "DUTSTATUS"
 
     def check_staf(self):
         '''
@@ -73,53 +74,141 @@ class STAF(object):
         
         return True
         
+    def _staf_handle_submit(self, location, service, request):
+        assert(not self.staf_handle is None, "Need create staf handle first")
+        result = self.staf_handle.submit(location, service, request)
+        if (result.rc != 0):
+            print("Error submitting request, RC: %d, Result: %s" % (result.rc, result.result))
+            print("Location: %s, Service: %s, Request: %s" % (location, service, request))
+            return False
+        
+        return result.result
         
     ########################################
     #
     # Queue Service
+    #     We use STAF Queue to manage all tasks to be executed on clients
     #
     ########################################
 
-    def add_message(self, message):
+    def add_message(self, ID, message, priority):
         '''
         Add message to handle message queue
         '''
+        location = 'local'
+        service = 'Queue'
+        request = 'QUEUE MESSAGE %s HANDLE %s NAME %s' % (message, self.staf_handle_id, priority, ID) 
         
-        
-    def get_message(self, message):
+        return self._staf_handle_submit(location, service, request)
+            
+    def get_message(self):
         '''
-        retrieve and removes message form handle message queue
+        retrieve and removes oldest message form handle message queue
         '''
+        location = 'local'
+        service = 'Queue'
+        request = 'GET FIRST 1'
         
+        return self._staf_handle_submit(location, service, request)
         
-    def peek_message(self, message):
+    def peek_message(self):
         '''
-        retrieve message form handle message queue
+        retrieve oldest message form handle message queue
         '''
+        location = 'local'
+        service = 'Queue'
+        request = 'PEEK FIRST 1'
         
+        return self._staf_handle_submit(location, service, request)
         
-    def delete_message(self, message):
+    def delete_message(self, ID):
         '''
         delete message form handle message queue
         '''
+        location = 'local'
+        service = 'Queue'
+        request = 'DELETE NAME %s' % ID
         
-    def list_message(self, message):
+        return self._staf_handle_submit(location, service, request)
+        
+    def list_message(self):
         '''
         retrieve the entire contents from handle message queue
         '''
+        location = 'local'
+        service = 'Queue'
+        request = 'LIST'
+        
+        return self._staf_handle_submit(location, service, request))
         
     ########################################
     #
     # Process Service
+    #     Used to execute tasks on clients
     #
     ########################################
         
-    def start_process(self, DUT, process):
+    def start_process(self, DUT, command):
         '''
         start a process
         '''
+        location = '%s' % DUT
+        service = 'Process'
+        request = 'START COMMAND %s ' % command
+        
+        return self._staf_handle_submit(location, service, request)
         
     def stop_process(self, DUT, process):
         '''
         stop a process
         '''
+        location = '%s' % DUT
+        service = 'Process'
+        request = 'STOP ALL'
+        
+        return self._staf_handle_submit(location, service, request)
+        
+    ########################################
+    #
+    # monitor and trust service
+    #     Used to record client status, 
+    #     and simulate a lock function to prevent client controlled by others
+    #
+    ########################################
+    
+    def set_DUT_status(self, DUT, status):
+        '''
+        '''
+        location = '%s' % DUT
+        service = 'MONITOR'
+        request = 'LOG MESSAGE %s NAME %s' % (status, self.monitor_name)
+        
+        return self._staf_handle_submit(location, service, request)
+        
+    def get_DUT_status(self, DUT):
+        '''
+        '''
+        location = '%s' % DUT
+        service = 'MONITOR'
+        request = 'QUERY MACHINE %s NAME %s' % (, self.monitor_name)
+        
+        return self._staf_handle_submit(location, service, request)
+        
+    def lock_DUT(self, DUT):
+        '''
+        '''
+        location = '%s' % DUT
+        service = 'TRUST'
+        request = 'SET DEFAULT 3'
+        
+        return self._staf_handle_submit(location, service, request)
+        
+    def release_DUT(self, DUT):
+        '''
+        '''
+        location = '%s' % DUT
+        service = 'TRUST'
+        request = 'SET DEFAULT 5'
+        
+        return self._staf_handle_submit(location, service, request)
+    
