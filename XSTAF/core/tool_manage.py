1dcfdeb03304b01e552b310b167cd9cd7f32faed
xcgspring <xcgspring@163.com>
2015-03-20 16:51:01 +0800

    refine tool manager
    Signed-off-by: xcgspring <xcgspring@163.com>

--- XSTAF/core/tool_manage.py
+++ XSTAF/core/tool_manage.py
@@ -26,27 +26,21 @@ class ToolManager(object):
         if not os.path.isabs(tools_location):
             #check if tools location exist in XSTAF path
             XSTAF_path = os.path.dirname(os.path.abspath(__file__))
-            abs_tools_location = os.path.join(XSTAF_path, "..", tools_location)
+            self.abs_tools_location  = os.path.join(XSTAF_path, "..", tools_location)
         else:
-            abs_tools_location = tools_location
-                
-        if not os.path.isdir(abs_tools_location):
-            LOGGER.warning("Can not find tools location: %s", abs_tools_location)
-            return
-            
-        #append abs tools path to python lib path
-        #so we can dynamic import them
-        self.abs_tools_location = abs_tools_location
-        sys.path.append(abs_tools_location)
-        
+            self.abs_tools_location  = tools_location
+
         #try get tools name list from pickle file
-        pickle_config_file = os.path.join(abs_tools_location, self.settings["ToolsConfigureFile"])
-        if not os.path.isfile(pickle_config_file):
-            LOGGER.warning("Can not find config file: %s", pickle_config_file)
-        self.pickle_config_file = pickle_config_file
+        self.pickle_config_file = os.path.join(self.abs_tools_location, self.settings["ToolsConfigureFile"])
         self.load_config()
 
     def get_tool(self, tool_module_name):
+        if not os.path.isdir(self.abs_tools_location):
+            LOGGER.warning("Can not find tools location: %s", self.abs_tools_location)
+            return None
+        #append abs tools path to python lib path
+        #so we can dynamic import them
+        sys.path.append(self.abs_tools_location)
         try:
             tool_module = __import__(tool_module_name)
             #want to reload the tool if tool is updated
@@ -60,22 +54,33 @@ class ToolManager(object):
             return tool
             
     def load_config(self):
+        if not os.path.isfile(self.pickle_config_file):
+            LOGGER.warning("Can not find config file: %s", self.pickle_config_file)
+            return
         #we load tool names from pickle file
         if os.path.isfile(self.pickle_config_file):
             with open(self.pickle_config_file, 'r') as f:
                 self.tool_name_list = pickle.load(f)
         
     def save_config(self):
+        if not os.path.isdir(self.abs_tools_location):
+            LOGGER.warning("Can not find tools location: %s", self.abs_tools_location)
+            return None
         #we save current tool names to pickle file
         with open(self.pickle_config_file, 'w') as f:
             pickle.dump(self.tool_name_list, f)
         
     @property
     def available_tool_name_list(self):
-        #check all packages under abs_tools_location
-        for name in os.listdir(self.abs_tools_location):
-            #only check dirs
-            abs_name = os.path.join(self.abs_tools_location, name)
-            if os.path.isdir(abs_name):
-                if not(name in self.tool_name_list) and not(self.get_tool(name) is None):
-                    yield name
+        if not os.path.isdir(self.abs_tools_location):
+            LOGGER.warning("Can not find tools location: %s", self.abs_tools_location)
+            while False:
+                yield None
+        else:
+            #check all packages under abs_tools_location
+            for name in os.listdir(self.abs_tools_location):
+                #only check dirs
+                abs_name = os.path.join(self.abs_tools_location, name)
+                if os.path.isdir(abs_name):
+                    if not(name in self.tool_name_list) and not(self.get_tool(name) is None):
+                        yield name
